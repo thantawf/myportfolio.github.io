a3426cb9809006e0c1f4c00f147ab37180378a82
Thanish Thawfeek <154671945+thantawf@users.noreply.github.com>
2025-01-05 12:29:49 +0530

    Add files via upload

---
+++ common.js
@@ -0,0 +1,7 @@
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
